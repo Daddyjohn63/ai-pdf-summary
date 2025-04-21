6ea6a1393cb987db04674bb48f89ed7503d78e42
Daddyjohn63 <john@webandprosper.co.uk>
2025-04-21 13:50:29 +0100

    added form and zod schema, submisssion is working to console.

---
+++ src/components/upload/upload-form.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { UploadFormInput } from './upload-form-input';
+import { z } from 'zod';
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: 'Invalid file' })
+    .refine(
+      file => file.size <= 20 * 1024 * 1024,
+      'File size must be less than 20MB'
+    )
+    .refine(
+      file => file.type.startsWith('application/pdf'),
+      'File must be a PDf'
+    )
+});
+
+export const UploadForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('submitted');
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const file = formData.get('file') as File;
+    console.log(file);
+
+    //validate the fields
+    const validatedFields = schema.safeParse({ file });
+
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid file'
+      );
+      return;
+    }
+
+    console.log(validatedFields.data);
+
+    //schema with zod
+    //upload the file to uploadthing
+    //parse the pdf using lang chain
+    //summarize the pdf using AI
+    //save the summary to the database
+    //redirect to the summary page
+  };
+
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto mt-2">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
+};
